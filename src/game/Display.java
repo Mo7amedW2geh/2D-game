c14212ab3bf0685311ee06479dd338881cff4ccb
Mohamed Wageh <mohamed.tyee333@gmail.com>
2024-10-27 19:46:09 +0300

    2D game

---
+++ src/game/Display.java
@@ -0,0 +1,64 @@
+package game;
+
+import object.SuperObject;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+public class Display extends JPanel {
+    private JFrame window;
+    private final String title;
+    private final int screenWidth;
+    private final int screenHeight;
+    private final Game game;
+
+    public Display(String title, int width, int height, Game game){
+        this.title = title;
+        this.screenWidth = width;
+        this.screenHeight = height;
+        this.game = game;
+
+        createDisplay();
+    }
+
+    public void createDisplay(){
+        window = new JFrame(title);
+        window.setSize(screenWidth, screenHeight);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.white);
+        this.setDoubleBuffered(true);
+        this.setFocusable(true);
+
+        window.add(this);
+        window.pack();
+    }
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+
+        Graphics2D g2d = (Graphics2D) g;
+
+        //Tiles
+        game.tileManager.draw(g2d);
+        //Objects
+        for (SuperObject object : game.objects) {
+            if (object != null) object.draw(g2d, game);
+        }
+        //Player
+        game.player.draw(g2d);
+
+        g2d.dispose();
+
+    }
+
+    public JFrame getWindow(){ return window; }
+}
---
+++ src/imageStaff/ImageLoader.java
@@ -0,0 +1,16 @@
+package imageStaff;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.util.Objects;
+
+public class ImageLoader {
+    public static BufferedImage loadImage(String path){
+        try{
+            return ImageIO.read(Objects.requireNonNull(ImageLoader.class.getResourceAsStream(path)));
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
